0ee424d94336583b2c40bca136fc956d9da74c27
olivertembo <tembokoliver3@gmail.com>
2021-11-26 13:30:37 +0300

    Update webpack.config.js
    configure  webpack.config.js to resolve typescript extensions

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,7 @@ module.exports = {
         alias: {
             '@': path.resolve('resources/js'),
         },
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
     },
 };
